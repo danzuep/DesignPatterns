8a677e882d67658e92f8e7772283bfb4c855df98
Daniel Collingwood <d.collingwood@truepartnercapital.com>
2023-08-17 17:32:13 +0800

    Composite pattern

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using DesignPatterns.Structural.Composite.GenericComponent;
+using DesignPatterns.Structural.Composite.AbstractComponent;
+using DesignPatterns.Structural.Composite.Totaliser;
+
+namespace DesignPatterns
+{
+    /// <summary>
+    /// Composite Design Pattern
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //CompositeInstance.Test();
+            //Parent.Test();
+            GenericComposite.Test();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+﻿# Design Patterns
+
+Repository for experimenting with various design patterns.
+
+## Structural
+
+### Composite
+
+* [Component](Structural/Composite/Component.md)
+* [Totaliser](Structural/Composite/Totaliser.md)
+
+<!--
+To display PlantUML previews in Visual Studio Code:  
+ 1. Extensions (Ctrl+Shift+X) -> Install "PlantUML".
+ 2. Click the gear icon -> Extension Settings.
+ 3. "plantuml.server": "https://kroki.example.com/plantuml".
+ 4. Ctrl+Shift+V to view.
+-->
